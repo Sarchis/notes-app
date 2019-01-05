db97a8ee5b16ed12c0b96daf1a92b62b9828b3fd
Santiago <santiago.riosv@gmail.com>
2019-01-05 12:04:57 -0500

    Se crea metodo para encriptar password

--- src/models/User.model.js
+++ src/models/User.model.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const { Schema } = mongoose;
 
@@ -7,6 +8,16 @@ const UserSchema = new Schema({
     email: { type: String, required: true },
     password: { type: String, required: true },
     date: { type: Date, default: Date.now }
-})
+});
+
+UserSchema.methods.encryptPassword = async (password) => {
+    const salt = await bcrypt.genSalt(10);
+    const hash = bcrypt.hash(password, salt);
+    return hash;
+}
+
+UserSchema.methods.matchPassword = async function (password) {
+    return await bcrypt.compare(password, this.password);
+}
 
 module.exports = mongoose.model('User', UserSchema);
--- src/routes/users.js
+++ src/routes/users.js
@@ -30,10 +30,20 @@ router.post('/users/signup', async (req, res) => {
     if(errors.length > 0){
         res.render('users/signup', { errors, name, email, password, confirm_password})
     } else {
-        const newUser = new User({ name, email, password, confirm_password });
-        await newUser.save()
-        console.log(newUser);
-        res.redirect('/users/signin');
+        const emailUser = await User.findOne({email: email});
+        if(emailUser){
+            console.log(emailUser);
+            // req.flash('error_msg', 'El correo está en uso')
+            // errors.push({text: 'El correo ya está en uso'})
+            res.redirect('/users/signup')
+        }else{
+            const newUser = new User({ name, email, password });
+            newUser.password = await newUser.encryptPassword(password);
+            await newUser.save();
+            req.flash('success_msg', 'Estás registrado')
+            res.redirect('/users/signin');
+            console.log(newUser);
+        }
     }
 
 })
