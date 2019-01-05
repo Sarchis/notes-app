9411738ef25f29ced40fb95eb3d3dde82b5ad46d
Santiago <santiago.riosv@gmail.com>
2019-01-04 22:08:19 -0500

    Se agrega el registro de usuarios, y almacenamiento en la BD

--- src/models/User.model.js
+++ src/models/User.model.js
@@ -6,7 +6,7 @@ const UserSchema = new Schema({
     name: { type: String, required: true },
     email: { type: String, required: true },
     password: { type: String, required: true },
-    confirm_password: { type: String, required: true }
+    date: { type: Date, default: Date.now }
 })
 
 module.exports = mongoose.model('User', UserSchema);
--- src/routes/users.js
+++ src/routes/users.js
@@ -13,12 +13,29 @@ router.get('/users/signup', (req, res) => {
     res.render('users/signup')
 })
 
+// Guardar usuario en la BD
 router.post('/users/signup', async (req, res) => {
     const { name, email, password, confirm_password } = req.body;
-    const newUser = new User({ name, email, password, confirm_password });
-    await newUser.save()
-    console.log(newUser);
-    res.send('Ok')
+    const errors = [];
+
+    if(name.length <= 0){
+        errors.push({text: 'Por favor, ingresa tu nombre'})
+    }
+    if(password != confirm_password){
+        errors.push({text: 'Las contraseñas no coinciden'})
+    }
+    if(password.length < 4){
+        errors.push({text: 'La contraseña debe ser mayor a 4 caracteres'})
+    }
+    if(errors.length > 0){
+        res.render('users/signup', { errors, name, email, password, confirm_password})
+    } else {
+        const newUser = new User({ name, email, password, confirm_password });
+        await newUser.save()
+        console.log(newUser);
+        res.redirect('/users/signin');
+    }
+
 })
 
 
